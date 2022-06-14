e6c0ce912522f9a624f6bac06cd882d3b7c840b1
Lance Daniel Crisang <lancecrisang14@gmail.com>
2022-06-14 09:31:55 +0800

    Organized IGraphics with regions

--- Cerulean.Common/Interfaces/IGraphics.cs
+++ Cerulean.Common/Interfaces/IGraphics.cs
@@ -8,13 +8,22 @@ namespace Cerulean.Common
 {
     public interface IGraphics
     {
+        #region INFORMATION
         public Size GetRenderArea(out int x, out int y);
         public void SetRenderArea(Size renderArea, int x, int y);
+        #endregion
 
+        #region TOOLS
         public void RenderClear();
         public void RenderPresent();
+        #endregion
 
+        #region PRIMITIVE DRAW
         public void DrawRectangle(int x, int y, Size size);
+        #endregion
+
+        #region TEXTURE DRAW
+        #endregion
         public virtual void Cleanup()
         {
 
