12da70b184f28f8c7c227650af64444e75bc7e77
Lance Daniel Crisang <lancecrisang14@gmail.com>
2022-06-19 05:11:19 +0800

    Add Input.Mouse static class

---
+++ Cerulean.Core/Input/Mouse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static SDL2.SDL;
+
+namespace Cerulean.Core.Input
+{
+    public static class Mouse
+    {
+        public static (int, int) GetGlobalMousePosition()
+        {
+            SDL_GetGlobalMouseState(out int x, out int y);
+            return (x, y);
+        }
+    }
+}
