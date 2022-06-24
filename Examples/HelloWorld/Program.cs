50e125d6620d41344c4da8d6ecfca4409de63ae2
Lance Crisang <lancecrisang14@gmail.com>
2022-06-25 06:25:57 +0800

    Update HelloWorld example

--- Examples/HelloWorld/Program.cs
+++ Examples/HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Cerulean.Core;
+using Cerulean.Common;
 using Cerulean.Components;
 
 // Set-up the CeruleanAPI singleton instance
@@ -9,22 +10,29 @@ var ceruleanApi = CeruleanAPI.GetAPI()
 
 // Create a layout from code.
 // NOTE: you should be using an XML layout file instead of this and running it through the layout compiler.
-Layout mainLayout = new();
-mainLayout.AddChild("Label", new Label()
+dynamic mainLayout = new Layout();
+mainLayout.AddChild("Grid", new Grid()
+{
+    RowCount = 2,
+    ColumnCount = 2
+});
+mainLayout.Grid.AddChild("Label", new Label()
 {
     Text = "Hello World!",
-    ForeColor = new(255, 255, 255),
+    ForeColor = new Color(255, 255, 255),
+    GridColumn = 1,
+    GridRow = 1,
     FontName = "Arial",
     FontStyle = "",
     FontSize = 24,
-    X = 120,
-    Y = 120
+    X = 0,
+    Y = 0
 });
 
 // Create a window that uses the layout.
 var mainWindow = ceruleanApi.CreateWindow(mainLayout);
 // Set its back color.
-mainWindow.BackColor = new(0, 0, 0);
+mainWindow.BackColor = new Color(0, 0, 0);
 
 // Wait for all windows to close and call Quit() on finish.
 ceruleanApi.WaitForAllWindowsClosed(true);
