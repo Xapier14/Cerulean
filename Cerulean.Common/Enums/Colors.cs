d5648a48f8ceb6c0c3989053f8011f776d76f364
Lance Crisang <lancecrisang14@gmail.com>
2022-06-25 22:37:34 +0800

    Seperate Colors enum from Color struct

---
+++ Cerulean.Common/Enums/Colors.cs
@@ -0,0 +1,8 @@
+namespace Cerulean.Common
+{
+    public static class Colors
+    {
+        public static readonly Color? None = null;
+        public static readonly Color Transparent = new(0, 0, 0, 0);
+    }
+}
