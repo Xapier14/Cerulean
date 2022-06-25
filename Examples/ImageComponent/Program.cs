d23964709fcfa2ff8ae85ab5fe9c905920a60c3b
Lance Crisang <lancecrisang14@gmail.com>
2022-06-25 22:40:21 +0800

    Add ImageComponent example

---
+++ Examples/ImageComponent/Program.cs
@@ -0,0 +1,19 @@
+﻿using Cerulean.Core;
+using Cerulean.Common;
+using Cerulean.Components;
+
+var ceruleanApi = CeruleanAPI.GetAPI()
+                             .UseSDL2Graphics()
+                             .UseConsoleLogger()
+                             .Initialize();
+
+var layout = new Layout();
+layout.AddChild("Image", new Image()
+{
+    FileName = "Cerulean.png",
+    PictureMode = PictureMode.None
+});
+
+var window = ceruleanApi.CreateWindow(layout);
+
+ceruleanApi.WaitForAllWindowsClosed(true);
