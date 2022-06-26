fe0d117fd7d7ff28aff7cc40e6a39c985eccc78d
Lance Crisang <lancecrisang14@gmail.com>
2022-06-26 11:04:46 +0800

    Add more Image component example

---
+++ Examples/ImageComponent-XML/.cerulean/layout0.cs
@@ -0,0 +1,132 @@
+using Cerulean;
+using Cerulean.Core;
+using Cerulean.Common;
+using Cerulean.Components;
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+// Generated with Cerulean-API Builder
+namespace Cerulean.App
+{
+	public partial class MainLayout : Layout
+	{
+		public MainLayout() : base()
+		{
+			AddChild("MainGrid", new Grid()
+			{
+				RowCount = 2,
+				ColumnCount = 3,
+			});
+			GetChildNullable("MainGrid")?.AddChild("Image1", new Image()
+			{
+				FileName = "Cerulean.png",
+				PictureMode = PictureMode.None,
+				GridRow = 0,
+				GridColumn = 0,
+			});
+			GetChildNullable("MainGrid")?.AddChild("Label1", new Label()
+			{
+				Text = "None",
+				FontName = "Arial",
+				FontSize = 28,
+				ForeColor = new Color(255, 0, 0),
+				X = 8,
+				Y = 8,
+				GridRow = 0,
+				GridColumn = 0,
+			});
+			GetChildNullable("MainGrid")?.AddChild("Image2", new Image()
+			{
+				FileName = "Cerulean.png",
+				PictureMode = PictureMode.Stretch,
+				GridRow = 0,
+				GridColumn = 1,
+			});
+			GetChildNullable("MainGrid")?.AddChild("Label2", new Label()
+			{
+				Text = "Stretch",
+				FontName = "Arial",
+				FontSize = 28,
+				ForeColor = new Color(255, 0, 0),
+				X = 8,
+				Y = 8,
+				GridRow = 0,
+				GridColumn = 1,
+			});
+			GetChildNullable("MainGrid")?.AddChild("Image3", new Image()
+			{
+				FileName = "Cerulean.png",
+				PictureMode = PictureMode.Center,
+				GridRow = 0,
+				GridColumn = 2,
+			});
+			GetChildNullable("MainGrid")?.AddChild("Label3", new Label()
+			{
+				Text = "Center",
+				FontName = "Arial",
+				FontSize = 28,
+				ForeColor = new Color(255, 0, 0),
+				X = 8,
+				Y = 8,
+				GridRow = 0,
+				GridColumn = 2,
+			});
+			GetChildNullable("MainGrid")?.AddChild("Image4", new Image()
+			{
+				FileName = "Cerulean.png",
+				PictureMode = PictureMode.Tile,
+				GridRow = 1,
+				GridColumn = 0,
+			});
+			GetChildNullable("MainGrid")?.AddChild("Label4", new Label()
+			{
+				Text = "Tile",
+				FontName = "Arial",
+				FontSize = 28,
+				ForeColor = new Color(255, 0, 0),
+				X = 8,
+				Y = 8,
+				GridRow = 1,
+				GridColumn = 0,
+			});
+			GetChildNullable("MainGrid")?.AddChild("Image5", new Image()
+			{
+				FileName = "Cerulean.png",
+				PictureMode = PictureMode.Fit,
+				GridRow = 1,
+				GridColumn = 1,
+			});
+			GetChildNullable("MainGrid")?.AddChild("Label5", new Label()
+			{
+				Text = "Fit",
+				FontName = "Arial",
+				FontSize = 28,
+				ForeColor = new Color(255, 0, 0),
+				X = 8,
+				Y = 8,
+				GridRow = 1,
+				GridColumn = 1,
+			});
+			GetChildNullable("MainGrid")?.AddChild("Image6", new Image()
+			{
+				FileName = "Cerulean.png",
+				PictureMode = PictureMode.Cover,
+				GridRow = 1,
+				GridColumn = 2,
+			});
+			GetChildNullable("MainGrid")?.AddChild("Label6", new Label()
+			{
+				Text = "Cover",
+				FontName = "Arial",
+				FontSize = 28,
+				ForeColor = new Color(255, 0, 0),
+				X = 8,
+				Y = 8,
+				GridRow = 1,
+				GridColumn = 2,
+			});
+		}
+	}
+}
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+// Generated on: 6/26/2022 10:35:57 AM
---
+++ Examples/ImageComponent-XML/Program.cs
@@ -0,0 +1,13 @@
+﻿using Cerulean.Core;
+
+// Set-up the CeruleanAPI singleton instance
+var ceruleanApi = CeruleanAPI.GetAPI()
+                             .UseSDL2Graphics()
+                             .UseConsoleLogger()
+                             .Initialize();
+
+// Create a layout from embedded layout "MainLayout".
+var window = ceruleanApi.CreateWindow("MainLayout");
+
+// Wait for all windows to close and call Quit() on finish.
+ceruleanApi.WaitForAllWindowsClosed(true);
