4e8d30bdf6225ef187bff4779152ae017cd92787
Lance Crisang <lancecrisang14@gmail.com>
2022-07-24 03:48:41 +0800

    Add common hooks

---
+++ Cerulean.Core/Hooks/DrawHooks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cerulean.Common;
+
+namespace Cerulean.Core
+{
+    public static class DrawHooks
+    {
+        public static void DrawViewportBorder(Component component, object[] args)
+        {
+            if (args.Length != 4)
+                return;
+
+            var graphics = (IGraphics)args[0];
+            var viewportSize = (Size)args[3];
+
+            graphics.DrawRectangle(0, 0, viewportSize, new Color("#0F0"));
+        }
+    }
+}
