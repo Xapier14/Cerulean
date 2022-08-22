1c230ac90de544e2801a7591affab02d4dff1def
Lance Crisang <lancecrisang14@gmail.com>
2022-08-22 20:29:18 +0800

    Update crn export

--- Examples/HelloWorld-XML/.cerulean/layout0.cs
+++ Examples/HelloWorld-XML/.cerulean/layout0.cs
@@ -4,33 +4,34 @@ using Cerulean.Common;
 using Cerulean.Components;
 #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-// Generated with Cerulean-API Builder
+// Generated with Cerulean.CLI 
 namespace Cerulean.App
 {
-	public partial class HelloWorldLayout : Layout
-	{
-		public HelloWorldLayout() : base()
-		{
-			AddChild("Grid", new Grid()
-			{
-				RowCount = 2,
-				ColumnCount = 2,
-				BackColor = new Color(0, 0, 0),
-			});
-			GetChildNullable("Grid")?.AddChild("Label", new Label()
-			{
-				Text = "Hello World!",
-				ForeColor = new Color(255, 255, 255),
-				GridColumn = 1,
-				GridRow = 1,
-				FontName = "Arial",
-				FontSize = 24,
-				X = 0,
-				Y = 0,
-			});
-		}
-	}
+    public partial class HelloWorldLayout : Layout
+    {
+        public HelloWorldLayout() : base()
+        {
+            var api = CeruleanAPI.GetAPI();
+            var resources = api.EmbeddedResources;
+            var styles = api.EmbeddedStyles;
+            AddChild("AnonymousComponent_637967969417929746", new Grid {
+                RowCount = 2,
+                ColumnCount = 2,
+                BackColor = new Color("#000"),
+            });
+            GetChild("AnonymousComponent_637967969417929746").AddChild("AnonymousComponent_637967969418050280", new Label {
+                Text = "Hello World!",
+                ForeColor = new Color("#FFF"),
+                GridColumn = 1,
+                GridRow = 1,
+                FontName = "Arial",
+                FontSize = 24,
+                X = 0,
+                Y = 0,
+            });
+        }
+    }
 }
 #pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
-// Generated on: 6/27/2022 9:37:39 AM
+// Generated on: 8/22/2022 8:29:01 PM
